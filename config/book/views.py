98f70ba49ef58cd8175d147fe613dcb3dab297aa
yatskevich90 <yatskevich90@inbox.ru>
2023-01-22 02:02:40 +0300

    Create_model_Articles

--- config/book/views.py
+++ config/book/views.py
@@ -1,3 +1,14 @@
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("Book")
+
+
+def about(request):
+    return HttpResponse("about")
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound("Not Found")
