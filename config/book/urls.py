e1673953bd0779a3a8143234054b5b49b995eb9b
yatskevich90 <yatskevich90@inbox.ru>
2023-01-26 02:15:44 +0300

    Login and Regist

--- config/book/urls.py
+++ config/book/urls.py
@@ -1,16 +1,16 @@
 
 from django.urls import path
-from .views import index, about, addpage, contact, login, show_post
+from .views import about, ArticlesHome, ShowPost, AddPage,\
+    RegisterUser, logout_user, LoginUser, ContactFormView
 
 urlpatterns = [
-    path('', index, name='home'),
+    path('', ArticlesHome.as_view(), name='home'),
     path('about/', about, name='about'),
-    path('addpage/', addpage, name='add_page'),
-    path('contact/', contact, name='contact'),
-    path('login/', login, name='login'),
-    path('post/<slug:post_slug>/', show_post, name='post')
-    # path('logout/', logout_user, name='logout'),
-    # path('register/', RegisterUser.as_view(), name='register'),
-    # path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    # path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
+    path('addpage/', AddPage.as_view(), name='add_page'),
+    path('contact/', ContactFormView.as_view(), name='contact'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', logout_user, name='logout'),
+    path('register/', RegisterUser.as_view(), name='register'),
+    path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
+
 ]
--- config/book/views.py
+++ config/book/views.py
@@ -1,51 +1,160 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, FormView
+
+from .forms import AddPostForm, RegisterUserForm, LoginUserForm, ContactForm
 from .models import Articles
-
-menu = [{'title': "Обо мне", 'url_name': 'about'},
-        {'title': "Добавить пост", 'url_name': 'add_page'},
-        {'title': "Обратная связь", 'url_name': 'contact'},
-        {'title': "Войти", 'url_name': 'login'}
-]
+from .utils import DataMixin, menu
 
 
-def index(request):
-    posts = Articles.objects.all()
-    data = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Главная страница'
-    }
-    return render(request, 'book/index.html', data)
+class ArticlesHome(DataMixin, ListView):
+    paginate_by = 3
+
+    model = Articles
+    template_name = 'book/index.html'
+    context_object_name = "posts"
+
+    def get_context_data(self, *, object_list=None, **kwargs):      #Формирует статический и динамический контекст#
+        context = super().get_context_data(**kwargs)                # получаем существуещий контекст
+        c_def = self.get_user_context(title="Главная страница")
+        return dict(list(context.items()) + list(c_def.items()))    #объединяем два словаря#
+
+    def get_queryset(self):
+        return Articles.objects.filter(is_published=True)           #отображает все отмеченые публикации#
+
+
+# def index(request):
+#     posts = Articles.objects.all()
+#     data = {
+#         'posts': posts,      отображение через функции
+#         'menu': menu,
+#         'title': 'Главная страница'
+#     }
+#     return render(request, 'book/index.html', data)
+
 
 
 def about(request):
-    return render(request, 'book/about.html', {'title': 'Обо мне', 'menu': menu})
+    contact_list = Articles.objects.all()
+    paginator = Paginator(contact_list, 3)
+
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'book/about.html', {'page_obj': page_obj, 'menu': menu, 'title': 'О сайте'})
 
 
-def addpage(request):
-    return HttpResponse("Форма по добавление статьи")
+class AddPage(DataMixin, CreateView, LoginRequiredMixin):
+    form_class = AddPostForm
+    template_name = 'book/addpage.html'
+    success_url = reverse_lazy('home')
+    login_url = reverse_lazy('home')
+    raise_exception = True                     #для авторизации#
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Добавление поста")
+        return dict(list(context.items()) + list(c_def.items()))
 
 
-def contact(request):
-    return HttpResponse("Обратная связь")
+    # def addpage(request):
+#     if request.method == 'POST':
+#         form = AddPostForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             # print(form.cleaned_data)
+#             form.save()
+#             return redirect('home')
+#     else:
+#         form = AddPostForm()
+#     return render(request, 'book/addpage.html', {'form': form, 'menu': menu, 'title': 'Добавление статьи'})
 
 
-def login(request):
-    return HttpResponse("Авторизация")
+# def contact(request):
+#     return HttpResponse("Обратная связь")
+
+class ContactFormView(DataMixin, FormView):
+    form_class = ContactForm
+    template_name = 'book/contact.html'
+    success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Обратная связь")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return redirect('home')
+
+# def login(request):
+#     return HttpResponse("Авторизация")
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound("Not Found")
 
 
-def show_post(request, post_slug):
-    post = get_object_or_404(Articles, slug=post_slug)
-    context = {
-        'post': post,
-        'menu': menu,
-        'title': post.title,
-        'cat_selected': post,
-    }
+class ShowPost(DataMixin, DetailView):
+    model = Articles
+    template_name = 'book/post.html'
+    pk_url_kwarg = 'post_slug'
+    context_object_name = 'post'
 
-    return render(request, 'book/post.html', context=context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title=context['post'])
+        return dict(list(context.items()) + list(c_def.items()))
+
+
+
+
+
+# def show_post(request, post_slug):
+#     post = get_object_or_404(Articles, pk=post_slug)
+#
+#     context = {
+#         'post': post,
+#         'menu': menu,
+#         'title': post.title,
+#         'cat_selected': post,
+#     }
+#
+#     return render(request, 'book/post.html', context=context)
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'book/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Регистрация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'book/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title="Авторизация")
+        return dict(list(context.items()) + list(c_def.items()))
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
